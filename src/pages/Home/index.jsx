d605ee74dcc404f9021a9bec3ec895ceae4ed5e7
Thiago Maylon <tmaylon88@gmail.com>
2023-02-17 20:34:46 -0300

    configurações iniciais

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import { HomeStyle } from "./style"
+
+export const Home = () => {
+    return(
+        <HomeStyle>
+            <h1>Home</h1>
+        </HomeStyle>
+    )
+}
---
+++ src/pages/Home/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const HomeStyle = styled.main`
+    width: 100vw;
+    height: 100vh;
+    color: #FFFFFF;
+    background-color: #000000;
+`;
